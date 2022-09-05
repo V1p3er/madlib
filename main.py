a0e19de24fba04517e7c8904a9209d33a0c44e80
Arman Gholinezhad <cybertechik@protonmail.com>
2022-09-05 09:35:04 +0430

    pushing to server

---
+++ main.py
@@ -0,0 +1,9 @@
+videoGame = input("What video game do you like? ")
+enjoyment = input("How is your level of enjoyment? ")
+typeGame = input("what kind of game do you like? ")
+where = input("where should you go to get in shape? ")
+
+madlib = f"We are here to play {videoGame}, this game is \
+so {enjoyment}, I also like {typeGame} but i should go to {where} to make some gains"
+print(madlib)
+
